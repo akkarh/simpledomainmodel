7aba5f61404d1b96a1b8c35d757a6d5493f89a07
Harshitha Akkaraju <harshi.akkaraju@gmail.com>
2018-01-29 23:20:03 -0800

    Implemented Job class

--- SimpleDomainModel/SimpleDomainModel/main.swift
+++ SimpleDomainModel/SimpleDomainModel/main.swift
@@ -28,16 +28,16 @@ public struct Money {
     public var currency : String
     
     /*
-        0: USD
-        1: GBP
-        2: CAN
-        3: EUR
-    */
+     0: USD
+     1: GBP
+     2: CAN
+     3: EUR
+     */
     private let conversions : [String: [String: Double]] = ["USD": ["USD": 1.0, "GBP": 0.5, "CAN": 1.25, "EUR": 1.5],
                                                             "GBP": ["USD": 2.0, "GBP": 1.0, "CAN": 1.74, "EUR": 1.14],
                                                             "EUR": ["USD":1.24, "GBP": 0.88, "CAN": 1.53, "EUR": 1.0],
                                                             "CAN": ["USD":0.81, "GBP": 0.58, "CAN": 1.0, "EUR": 0.65]
-                                                   ]
+    ]
     
     init(amount: Int, currency: String) {
         self.amount = amount
@@ -89,58 +89,69 @@ open class Job {
     }
     
     open func calculateIncome(_ hours: Int) -> Int {
+        switch self.type {
+            case .Hourly(let rate) :
+                return Int(Double(hours) * rate)
+            case .Salary(let amount) :
+                return Int(amount)
+        }
     }
     
     open func raise(_ amt : Double) {
+        switch self.type {
+        case .Hourly(let rate) :
+            return Int((Double(hours) * rate) + (amt * (Double(hours) * rate)))
+        case .Salary(let amount) :
+             return Int(amount) + (Int(amount) * amt)
+        }
     }
 }
-
-////////////////////////////////////
-// Person
-//
-open class Person {
-    open var firstName : String = ""
-    open var lastName : String = ""
-    open var age : Int = 0
-    
-    fileprivate var _job : Job? = nil
-    open var job : Job? {
-        get { }
-        set(value) {
+    ////////////////////////////////////
+    // Person
+    //
+    open class Person {
+        open var firstName : String = ""
+        open var lastName : String = ""
+        open var age : Int = 0
+        
+        fileprivate var _job : Job? = nil
+        open var job : Job? {
+            get { }
+            set(value) {
+            }
+        }
+        
+        fileprivate var _spouse : Person? = nil
+        open var spouse : Person? {
+            get { }
+            set(value) {
+            }
+        }
+        
+        public init(firstName : String, lastName: String, age : Int) {
+            self.firstName = firstName
+            self.lastName = lastName
+            self.age = age
+        }
+        
+        open func toString() -> String {
         }
     }
     
-    fileprivate var _spouse : Person? = nil
-    open var spouse : Person? {
-        get { }
-        set(value) {
+    ////////////////////////////////////
+    // Family
+    //
+    open class Family {
+        fileprivate var members : [Person] = []
+        
+        public init(spouse1: Person, spouse2: Person) {
         }
-    }
-    
-    public init(firstName : String, lastName: String, age : Int) {
-        self.firstName = firstName
-        self.lastName = lastName
-        self.age = age
-    }
-    
-    open func toString() -> String {
-    }
-}
-
-////////////////////////////////////
-// Family
-//
-open class Family {
-    fileprivate var members : [Person] = []
-    
-    public init(spouse1: Person, spouse2: Person) {
-    }
-    
-    open func haveChild(_ child: Person) -> Bool {
-    }
-    
-    open func householdIncome() -> Int {
-    }
+        
+        open func haveChild(_ child: Person) -> Bool {
+        }
+        
+        open func householdIncome() -> Int {
+        }
 }
 
 
